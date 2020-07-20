7dc9b9189dafc3740cc8d8320c519d3dec86e4f2
Yonghyun <lygon55555@gmail.com>
2020-07-20 13:57:18 +0900

    base setting

---
+++ PIP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PIP
+//
+//  Created by Yonghyun on 2020/07/20.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
