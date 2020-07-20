4c2960c1c32113ae0a8382001e6e469720ae22b9
Yonghyun <lygon55555@gmail.com>
2020-07-20 14:01:03 +0900

    pip view

--- PIP/ViewController.swift
+++ PIP/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var pipView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
